f6da642604ac29ad5c50271a075c5b183e127dd7
Administrator <757735187@qq.com>
2019-10-11 16:12:13 +0800

    test跨越

---
+++ src/network/request.js
@@ -0,0 +1,27 @@
+//使用拦截器
+import axios from 'axios'
+export function request(config) {
+    /*1.创建axios的实例*/
+    const instance  = axios.create({
+       baseURL: 'http://127.0.0.1:8080',
+        timeout: 5000
+    })
+   //2.asios的拦截器
+    //asios.interceptors 拦截全局的
+    //instance.interceptors 只拦截当前实例
+    //instance.interceptors.request 拦截请求
+    //instance.interceptors.response 拦截响应的
+    instance.interceptors.request.use(config => {
+        console.log(config);
+        return config
+    },err => {
+        console.log(err);
+    })
+    //响应拦截
+    instance.interceptors.response.use(res =>{
+        return res.data
+    },error => {
+        console.log(err);
+    })
+    return instance(config)
+}
---
+++ src/views/test.vue
@@ -0,0 +1,34 @@
+<template>
+      <div class="test">
+          <button @click="test">测试</button>
+          <h2>{{hhh}}</h2>
+      </div>
+</template>
+
+<script>
+import {request} from "../network/request";
+
+export default {
+        name: "test",
+        data(){
+            return{
+          hhh:''
+            }
+        },
+        methods:{
+            test(){
+                request({
+                    url:'user'
+                }).then(res =>{
+                    this.hhh = res
+                    console.log('hhh');
+                })
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
